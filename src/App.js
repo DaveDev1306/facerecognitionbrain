3d3b19bd893cb93a6791529d884677e31b6ed7b6
David Magueijo <davedev1984@gmail.com>
2020-11-12 14:04:21 +0000

    Tweaking things

--- src/App.js
+++ src/App.js
@@ -85,7 +85,7 @@ class App extends Component {
       .then(response => response.json())
         .then(response => {
           if (response) {
-            fetch('https://git.heroku.com/guarded-river-62949.git:3000/image', {
+            fetch('https://git.heroku.com/guarded-river-62949.git/image', {
               method: 'put',
               headers: {'Content-Type': 'application/json'},
               body: JSON.stringify({
